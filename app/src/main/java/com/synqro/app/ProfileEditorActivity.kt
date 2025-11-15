8761db48b4b9b88fdee666a00005a8e0764a8a7b
JeddoreEnviroshake <jeddore.mcdonald@enviroshake.com>
2025-11-15 00:21:51 -0500

    Add ProfileEditorActivity and navigation button

---
+++ app/src/main/java/com/synqro/app/ProfileEditorActivity.kt
@@ -0,0 +1,12 @@
+package com.synqro.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfileEditorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_editor)
+    }
+}
